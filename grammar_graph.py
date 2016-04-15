fdbc50ab2fb8fb1634262072668d3ed840db3221
Alexander Bikadorov <bikaejkb@mailbox.tu-berlin.de>
2016-04-15 17:11:48 +0200

    applied style guides for classes

--- grammar_graph.py
+++ grammar_graph.py
@@ -21,26 +21,26 @@ class Node:
     "One node in graph. Representing a syntax symbol"
     def __init__(self, value):
         self.value = value
-        self.child_nodes = []
-        self.p = -1
+        self._child_nodes = []
+        self._p = -1
 
     def set_edges(self, op, children, p=-1):
-        self.op = op
-        self.child_nodes = children
-        self.p = p
+        self._op = op
+        self._child_nodes = children
+        self._p = p
 
     def traverse(self, level=0):
-        if self.op == Operation.or_:
-            rc = 0 if random.random() <= (self.p ** (1.0 / level)) else 1
-            return self.child_nodes[rc].traverse(level + 1)
+        if self._op == Operation.or_:
+            rc = 0 if random.random() <= (self._p ** (1.0 / level)) else 1
+            return self._child_nodes[rc].traverse(level + 1)
         else:
-            return [child.traverse(level + 1) for child in self.child_nodes]
+            return [child.traverse(level + 1) for child in self._child_nodes]
 
     def __repr__(self):
-        return "<Node V:" + str(self.value) + " C:" + str(self.child_nodes) + ">"
+        return "<Node V:" + str(self.value) + " C:" + str(self._child_nodes) + ">"
 
     def __str__(self):
-        return str(self.value) + "#:" + str(self.child_nodes)
+        return str(self.value) + "#:" + str(self._child_nodes)
 
 
 class Literal(Node):
@@ -56,4 +56,4 @@ class Literal(Node):
         return "<Literal L:" + str(self.value) + ">"
 
     def __str__(self):
-        return str(self.value) + "#:" + str(self.child_nodes)
+        return str(self.value) + "|"
